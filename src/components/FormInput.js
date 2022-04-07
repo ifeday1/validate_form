c1c06d717c5f17783784b670167759ab197576fd
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-04-07 04:20:23 +0100

    concluding part, using Regex validation method

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./formInput.css"
 
 const FormInput = (props) => {
-    const {label, onChange, id, ...inputProps} = props;
+  const [focused, setFocused] = useState(false);
+    const {label, errorMessage, onChange, id, ...inputProps} = props;
+
+    const handleFocus= (e)=>{
+      setFocused(true);
+    };
+
+
   return (
     <div className='formInput'>
     <label>{label}</label> 
-    <input {...inputProps} onChange={onChange} ></input>
+    <input {...inputProps} onChange={onChange} 
+    onBlur={handleFocus} 
+    onFocus={()=>inputProps.name==="confirmPassword" && setFocused(true)}
+    focused={focused.toString()}
+    ></input>
+    <span>{errorMessage}</span>
     </div>
   )
 }
